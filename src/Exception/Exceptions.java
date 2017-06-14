c8b75d952d6f7fe533b2a2ea32f5b1cad316eef5
Nitish2 <nitishn886@gmail.com>
2017-06-14 15:47:24 +0530

    Assignment11.1

---
+++ src/Exception/Exceptions.java
@@ -0,0 +1,20 @@
+package Exception;
+
+/**
+ * Trying to generate Exception .
+ * By initializing array of 5 elements and prints the 6th element . 
+ * @author HP
+ *
+ */
+public class Exceptions {
+
+	public static void main(String[] args) {
+
+		String Languages[] = { "C", "C++", "Java", "Perl", "Python" };
+		
+			System.out.println(Languages[6]);
+
+		
+
+	}
+}
